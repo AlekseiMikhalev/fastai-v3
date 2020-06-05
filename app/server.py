1c99c486acfb3d62dad08e2767e366f2fe9d2350
Aleksei Mikhalev <aleks_mihalev@list.ru>
2020-06-05 03:04:10 +0300

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,8 +9,8 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.googleapis.com/drive/v3/files/1Se1DHWmrd8mH7iJ0n9lgHkUXCBPMrl5v?alt=media&key=AIzaSyBREAY60pECgox-JsadDj7mWbhlETmzcSs'
-export_file_name = 'resnet152.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1ZsdDM2OIV_O4ZNRRfPCPA4_0Uvvko2r6'
+export_file_name = 'vgg16.pkl'
 
 classes = ['positive', 'negative']
 path = Path(__file__).parent
